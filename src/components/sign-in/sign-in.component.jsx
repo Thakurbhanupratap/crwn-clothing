49068becc29d8acb18a7f61348d40930830146af
Bhanu Pratap Singh <bhanupratap983784@gmail.com>
2021-09-27 16:27:38 +0530

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom.button.component";
-
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -48,7 +48,12 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <CustomButton type="submit">Sign in </CustomButton>
+          <div className="custom-buttons">
+            <CustomButton type="submit">Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in With Google{" "}
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+// import firebase from "firebase/app";
+// import * as firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "AIzaSyA8mygoJnoQO0hMhM682evcDxYAqlb4mNY",
+  authDomain: "crwn-db-73d3d.firebaseapp.com",
+  projectId: "crwn-db-73d3d",
+  storageBucket: "crwn-db-73d3d.appspot.com",
+  messagingSenderId: "246112940267",
+  appId: "1:246112940267:web:79d4a9f13d58382a8f8eae",
+  measurementId: "G-R9BDKMXC9P",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
